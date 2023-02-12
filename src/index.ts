e7e86cde35e3da12748077e9dffb413cc19a4fdb
shinyay <shinya.com@gmail.com>
2023-02-12 22:20:05 +0900

    Add standard in and out

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ const message: string = "Hello, TypeScript!";
 
 console.log(message);
 
+import { createInterface } from "readline";
+
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
