59aa42dfd69d9e1fcda8f783c1769414929bbd88
shinyay <shinya.com@gmail.com>
2023-02-12 22:23:34 +0900

    Add standard in and out

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,8 @@ const readline = createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+readline.question('Input characters:', (line) => {
+    console.log(`INPUT: ${line}`);
+    readline.close();
+});
